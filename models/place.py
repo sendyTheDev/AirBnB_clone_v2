271c322f8ec32260e0f266d6f6bf5b260ad60e21
sendyTheDev <phathisanisendy@gmail.com>
2023-09-19 14:00:34 -0700

    to be fixed

--- models/place.py
+++ models/place.py
@@ -1,34 +1,56 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import Base
 from os import getenv
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
-
+from sqlalchemy.ext.declarative import declarative_base
 
 class Place(BaseModel):
     """ A place to stay """
     __tablename__ = 'places'
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        city_id = Coulum(String(60), ForeignKey("cities.id"), nullable=False)
-        user_id = Column(Strint(60), ForeignKey("users.id"), nullable=False)
-        name = Column(Strint(128), nullable=False)
-        description = Column(Strint(1024), nullable=False)
-        number_rooms = Column(Integer, nullable=False, default=0)
-        number_bathrooms = Column(Integer, nullable=False, default=0)
-        max_guest = Column(Integer, nullable=False, default=0)
-        price_by_night = Column(Integer, nullable=False, default=0)
-        latitude = Column(Integer, nullable=False)
-        longitude = Column(Integer, nullable=False)
+    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024))
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
+    latitude = Column(0)
+    longitude = Column(0)
+    amenity_ids = []
+
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        reviews = relationship("Review", cascade='all, delete, delete-orphan',
+                               backref="place")
+
+        amenities = relationship("Amenity", secondary=place_amenity,
+                                 viewonly=False,
+                                 back_populates="place_amenities")
     else:
-        city_id = ""
-        user_id = ""
-        name = ""
-        description = ""
-        number_rooms = 0
-        number_bathrooms = 0
-        max_guest = 0
-        price_by_night = 0
-        latitude = 0.0
-        longitude = 0.0
-        amenity_ids = []
+        @property
+        def reviews(self):
+            """ Returns list of reviews.id """
+            var = models.storage.all()
+            list_array = []
+            result = []
+            for key in var:
+                review = key.replace('.', ' ')
+                review = shlex.split(review)
+                if (review[0] == 'Review'):
+                    list_array.append(var[key])
+            for elem in list_array:
+                if (elem.place_id == self.id):
+                    result.append(elem)
+            return (result)
+        @property
+        def amenities(self):
+            """ Returns list of amenity ids """
+            return self.amenity_ids
+
+        @amenities.setter
+        def amenities(self, obj=None):
+            """ Appends amenity ids to the attribute """
+            if type(obj) is Amenity and obj.id not in self.amenity_ids:
+                self.amenity_ids.append(obj.id)
--- models/user.py
+++ models/user.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel, Base
-from os import getenv
+from models.base_model import Base
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
-
+from models.review import Review
+from models.place import Place
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
 
-    __tablename__ = 'users'
-
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=False)
-        last_name = Column(String(128), nullable=False)
-        places = relationship("place", cascade="all,delete", backref="user")
-    else:
-        email = ''
-        password = ''
-        first_name = ''
-        last_name = ''
+    __tablename__ = "users"
+    password = Column(String(128), nullable=False)
+    email = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="user")
+    reviews = relationship("Review", cascade='all, delete, delete-orphan',
+                           backref="user")
