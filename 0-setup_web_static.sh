cc532b0b3d15a5a3bb2705efca4451543c5d49db
joekariuki3 <joekariuki3@gmail.com>
2023-10-04 16:58:46 +0300

    Prepare your web servers

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# script to setup server for deployment of web_static
+sudo apt-get update -y
+sudo apt-get install nginx -y
+sudo mkdir -p /data/web_static/releases/test/
+sudo ln -f -s /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+echo "Sample text" > /data/web_static/releases/test/index.html
+sudo chown -R ubuntu:ubuntu /etc/nginx/sites-available/default
+sudo sed -i 's#root /var/www/html;#root /var/www/html;\n\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}#1' /etc/nginx/sites-available/default
+sudo service nginx restart
